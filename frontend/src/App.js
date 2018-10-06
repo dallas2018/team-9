6aa6574ebbbabacdb5860277ee7eb0288f5003dd
MickHoover <43889812+MickHoover@users.noreply.github.com>
2018-10-06 01:41:26 -0400

    Update frontend/src/App.js
    here is page2 link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import './App.css';
 import Login from './components/login-in';
-
+import page2 from './components/page2';
 class App extends Component {
   render() {
     return (
@@ -12,7 +12,7 @@ class App extends Component {
                 <Switch>
 
                     <Route exact name="home" path="/" component={Login}/>
-
+                    <Route exact name="page2" path="/page2" component={page2}/>
                 </Switch>
             </div>
         </Router>
